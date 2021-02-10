d245b1d509bedbf16120c0f0c5409b0345812375
Jules Perkins <julia.perkins405@gmail.com>
2021-02-10 11:55:10 -0500

    Changed styling, connected tags and blog to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,17 @@ import Button from "../components/button"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`jules`, `perkins`, `personal website`]} />
-    <h1>This is my ~future~ personal site!</h1>
-    <Button buttonText="An example button that does nothing" />
+    <h2>This is my ~future~ personal site!</h2>
+    {/* <Button buttonText="An example button that does nothing" /> */}
     <p>Clearly, this site is under construction.</p>
     <p>Soon, it will be something great!</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </div> */}
+    <Link to="/blog/">Head to the blog!</Link> <br />
+    <Link to="/tags/">See tags</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
+    {/* <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
 
