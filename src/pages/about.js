2857b0d19d73c4fcb3711f2550f0f38a9d947bfc
Jules Perkins <julia.perkins405@gmail.com>
2021-04-15 11:38:17 -0400

    Adding about me video

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,8 @@ import Layout from "../components/layout"
 const About = () => (
     <Layout>
       <h1>About Jules</h1>
-      <p>Yeah, here we are</p>
+      <p>Here is a video about me!</p>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/SoVR88ynJ_w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </Layout>
 )
 
