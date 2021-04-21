2eba844ddc4d91132fd21a0494d5d3084dbb1d93
Jules Perkins <julia.perkins405@gmail.com>
2021-04-21 19:41:01 -0400

    Adding image

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const IndexPage = () => (
     <Link to="/tags/">See tags</Link> */}
     {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
     {/* <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
-  </Layout>
+    </Layout>
 )
 
 export default IndexPage
